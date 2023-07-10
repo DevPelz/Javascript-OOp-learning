65bcd306bf187e31a362ec151187e40af41ee48b
pelzfx <pelzfx@gmail.com>
2023-07-10 11:58:47 +0100

    feat: add array method

--- starter/script.js
+++ starter/script.js
@@ -31,3 +31,23 @@ console.log(Person.prototype.isPrototypeOf(matilda));
 
 Person.prototype.species = 'Homo sapiens';
 console.log(jonas, matilda);
+console.log(jonas.hasownProperty('firstName'));
+console.log(jonas.hasownProperty('species'));
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [2,3,4,4,5,6,6,7,8];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function(){
+    return [...new Set(this)];
+};
+
+console.log(arr.unique());
