9d05d43735755f3e0a615319d08812a4418c0d69
pelzfx <pelzfx@gmail.com>
2023-07-10 09:20:30 +0000

    feat: add prototype

--- starter/script.js
+++ starter/script.js
@@ -3,6 +3,10 @@
 const Person = function(firstName, birthYear){
     this.firstName = firstName;
     this.birthYear = birthYear;
+    // Never add a method into a constructor function
+    // this.calcAge = function(){
+    //     console.log(2037 - this.birthYear);
+    // }
 }
 
 const jonas = new Person('Jonas', 1991);
@@ -11,3 +15,8 @@ console.log(jonas);
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
+
+// Prototypes
+Person.prototype.calcAge = function (){
+    console.log(2037 - this.birthYear);
+}
