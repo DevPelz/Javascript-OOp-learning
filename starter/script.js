e93f46eeb7849fc79bfb4c66b3ff9fa3045b758b
pelzfx <pelzfx@gmail.com>
2023-07-10 12:19:53 +0100

    feat: add ES6 classes

--- starter/script.js
+++ starter/script.js
@@ -80,3 +80,29 @@ bmw.accelerate();
 bmw.accelerate();
 bmw.brake();
 bmw.brake();
+
+// ES6 classes
+
+
+// class expression
+
+// const PersonCl = class {}
+
+// class declaration
+
+class PersonCl {
+    constructor(firstName, birthYear){
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
+
+    calcAge(){
+        console.log(2037 - this.birthYear);
+    };
+};
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
