68b3574b9bc6e8a605d2b82e823095ddcddd1fe5
pelzfx <pelzfx@gmail.com>
2023-07-10 09:10:47 +0000

    feat: add new features

--- starter/script.js
+++ starter/script.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const Person = function(firstName, birthYear){
-
+    this.firstName = firstName;
+    this.birthYear = birthYear;
 }
 
-new Person('Jonas', 1991);
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
